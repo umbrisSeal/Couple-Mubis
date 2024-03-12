dd711d92926318c811fd2a6f69b4ca46720573fa
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-03-12 17:05:46 -0600

    Added Perfil configuration page.
     - Fixed some routes in App router.
     - Added path to redirect user from /perfil to /configuracion when no ID is provided.
     - Added simulated data for Perfil component.
     - Moved simulated data to Route loader.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,56 @@ import Home from './pages/Home';
 import VerPelicula from './pages/VerPelicula';
 import Perfil from './pages/Perfil';
 
+const datosSimulados = {
+    nombre: 'Mac Giver',
+    id: 'RT0054AWSA',
+    xp: 210,
+    bibliografia: 'Hola! Soy Mac Gyver, autentico cineasta de los años 60. Vivi en una casa de campo hasta mis 50 años, ahora me dedico a criticar gente en internet.',
+    esAmigo: false,
+    esBloqueado: false,
+    listasPublicas: [
+        {nombre: 'La Toalla del Mojado', id: 'RFRTG31'},
+        {nombre: 'Mis Actuaciones de 1971', id: 'RTGAW21'},
+        {nombre: 'James Bond', id:'J5B4GG'},
+    ]
+}
 
 function App() {
+
+    const solicitarDatosUsuario = (id = 0) => {
+        // Si no se provee un id, entonces devolver datos del usuario actual. (por cookie?)
+        // Quien sea que use esto, debe de estar autenticado.
+        if(id == 0) {
+            // Datos del usuario.
+            return {    // Datos simulados
+                id: '4GT70ARR49',
+                nombre: 'Kevin Monterrey',
+                xp: 100,
+                alias: '',
+                bibliografia: '',
+                usarAlias: true,
+                usarPrivacidad: true,
+                usuariosBloqueados: [/* {id, nombreUsuario} */],
+                idiomaBusqueda: 0,
+            }
+        } else {
+            // Datos del usuario solicitado.
+            return {
+                nombre: 'Mac Giver',
+                id: 'RT0054AWSA',
+                xp: 514,
+                bibliografia: 'Hola! Soy Mac Gyver, autentico cineasta de los años 60. Vivi en una casa de campo hasta mis 50 años, ahora me dedico a criticar gente en internet.',
+                esAmigo: false,
+                esBloqueado: false,
+                listasPublicas: [
+                    {nombre: 'La Toalla del Mojado', id: 'RFRTG31'},
+                    {nombre: 'Mis Actuaciones de 1971', id: 'RTGAW21'},
+                    {nombre: 'James Bond', id:'J5B4GG'},
+                ]
+            }
+        }
+    }
+
 
     const router = createBrowserRouter(createRoutesFromElements([
         <Route path='/' element={<LandingPage />} />,
@@ -18,9 +66,11 @@ function App() {
         <Route path='/registro' element={<Login version='vincular' />} />,
         <Route path='/home' element={<Home />} />, /* agregar loader para authenticar usuario y sesion. */
         <Route path='/pelicula/:peliculaId' element={<VerPelicula />} />,
-        <Route path='pelicula' loader={() => redirect('/home') } />,    /* redirecciona al no especificar :peliculaid */
-        <Route path='perfil/:perfilId' element={<Perfil configuracion={false} />} />,
+        <Route path='/pelicula' loader={() => redirect('/home') } />,    /* redirecciona al no especificar :peliculaid */
+        <Route path='/perfil/:perfilId' element={<Perfil configuracion={false} />} loader={({params}) => solicitarDatosUsuario(params.perfilId)} />,
+        <Route path='/perfil' loader={() => redirect('/configuracion')} />,
         // Crear ruta para redireccionar al perfil de usuario si no se encontro id.
+        <Route path='/configuracion' element={<Perfil configuracion={true} />} loader={() => solicitarDatosUsuario()} />,
 
         <Route path='*' element={<ErrorPage />} />
     ]))
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,12 +1,28 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import '../styles/Perfil.css';
 import { obtenerNivel } from '../assets/js/niveles';
 import Header from '../components/Header';
 import Boton from '../components/Boton';
 
 function Perfil(props) {
+    const datosUsuario = useLoaderData();
     const [configuracion, setConfiguracion] = useState(props.configuracion || false);
+    const [alias, setAlias] = useState(datosUsuario.alias || '');
+    const [usarAlias, setUsarAlias] = useState(datosUsuario.usarAlias || false);
+    const [usarPrivacidad, setUsarPrivacidad] = useState(datosUsuario.usarPrivacidad || false);
+    const [bibliografia, setBibliografia] = useState(datosUsuario.bibliografia);
+    const [idiomaBusqueda, setIdiomaBusqueda] = useState(datosUsuario.idiomaBusqueda || 0);
+    const [borrarCuenta, setBorrarCuenta] = useState('');
+    const [caracteres, setCaracteres] = useState(datosUsuario.bibliografia.length);
+
+    const handleAliasChange = (event) => setAlias(event.target.value);
+    const handleBibliografiaChange = (event) => { setBibliografia(event.target.value); setCaracteres(event.target.value.length); }
+    const handleBorrarCuentaChange = (event) => setBorrarCuenta(event.target.value);
+    const handleIdiomaBusquedaChange = (event) => setIdiomaBusqueda(event.target.value);
+
+    const caracteresMaximos = 160;
+
 
     const datosSimulados = {
         nombre: 'Mac Giver',
@@ -22,40 +38,112 @@ function Perfil(props) {
         ]
     }
 
+    /*
+        Datos el usuario.
+        {    // Datos simulados.
+            idUsuario: '4GT70ARR49',
+            nombre: 'Kevin Monterrey',
+            xp: 100,
+            alias: '',
+            bibliografia: '',
+            usarAlias: true,
+            usarPrivacidad: true,
+            usuariosBloqueados: [// {id, nombreUsuario} ],
+            idiomaBusqueda: 0,
+        }
+        Datos de un usuario solicitado:
+        {
+            nombre: 'Mac Giver',
+            id: 'RT0054AWSA',
+            xp: 210,
+            bibliografia: 'Hola! Soy Mac Gyver, autentico cineasta de los años 60. Vivi en una casa de campo hasta mis 50 años, ahora me dedico a criticar gente en internet.',
+            esAmigo: false,
+            esBloqueado: false,
+            listasPublicas: [
+                {nombre: 'La Toalla del Mojado', id: 'RFRTG31'},
+                {nombre: 'Mis Actuaciones de 1971', id: 'RTGAW21'},
+                {nombre: 'James Bond', id:'J5B4GG'},
+            ]
+        }
+    */
+
     return <Fragment>
         <Header version='perfil' configuracion={configuracion} />
         <main className='fondo-rojo contenedor-informacion-perfil'>
             <div className='contenedor-informacion-perfil-perfil'>
-                <h1 className='negritas'> {datosSimulados.nombre} </h1>
-                <p> {`ID: ${datosSimulados.id}`} </p>
+                <h1 className='negritas'> {datosUsuario.nombre} </h1>
+                <p> {`ID: ${datosUsuario.id}`} </p>
                 <br />
-                <p> {`Nivel ${obtenerNivel(datosSimulados.xp).nivel}`} </p>
-                <h4 className='negritas'> {obtenerNivel(datosSimulados.xp).nombreNivel} </h4>
+                <p> {`Nivel ${obtenerNivel(datosUsuario.xp).nivel}`} </p>
+                <h4 className='negritas'> {obtenerNivel(datosUsuario.xp).nombreNivel} </h4>
                 <br />
-                <div id='perfil-botones'>
-                    <Boton version='perfilAgregar' />
-                    <Boton version='perfilBloquear' />
-                </div>
-                <br />
-                <br />
-                <p> Bibliografia: </p>
-                <p id='perfil-bibliografia'> {datosSimulados.bibliografia} </p>
-                <br />
-                <br />
-                <br />
-                <p> Listas Publicas: </p>
-                <div id='perfil-conjunto-listas'>
-                    {datosSimulados.listasPublicas.map((lista, index) => {
-                        return(
-                            <Link to={`/lista/${lista.id}`} className='no-hypervinculo' key={lista + index}>
-                                <div className='perfil-lista'>
-                                    <img src='../src/assets/images/iconos/pelicula-roja.png' alt='Icono Pelicula' width={50} />
-                                    <p> {lista.nombre} </p>
-                                </div>
-                            </Link>
-                        )
-                    })}
-                </div>
+
+                { configuracion ?
+                    <Fragment>
+                        <div className='contenedor-barraxp'>
+                            <p className='barraxp-datos'> {` ${obtenerNivel(datosUsuario.xp).xpRestante} XP / ${obtenerNivel(datosUsuario.xp).xpSiguienteNivel} XP `} </p>
+                            <div className='barraxp-fondo'>
+                                <div className='barraxp-relleno' style={{width: `${obtenerNivel(datosUsuario.xp).porcentaje}%`}}></div>
+                            </div>
+                        </div>
+                        <form className='grid-configuracion'>
+
+                            <label htmlFor='configuracion-alias'> Alias de Couple Mubis: </label>
+                            <input id='configuracion-alias' className='configuracion-input' name='configuracion-alias' placeholder='Ingresa un alias...' value={alias} onChange={handleAliasChange} maxLength={15} />
+
+                            <label> Mostrar Nombre de Usuario: </label>
+                            <p> Esto es un slider, en componente. </p>
+
+                            <label> Privacidad de Perfil: </label>
+                            <p> Esto es un slider, en componente. </p>
+
+                            <label htmlFor='configuracion-bibliografia'> Biografia de Perfil: </label>
+                            <div>
+                                <textarea id='configuracion-bibliografia' className='configuracion-input' name='configuracion-bibliografia' placeholder='¿Que peliculas te gustan? Compartelo con el mundo!... ' value={bibliografia} onChange={handleBibliografiaChange} maxLength={caracteresMaximos} />
+                                <span className='configuracion-indicador-caracteres'> {`${caracteres} / ${caracteresMaximos}`} </span>
+                            </div>
+
+                            <label htmlFor='configuracion-idioma'> Idioma de Busqueda: </label>
+                            <select id='configuracion-idioma' className='configuracion-input' name='configuracion-idioma' value={idiomaBusqueda} onChange={handleIdiomaBusquedaChange}>
+                                <option value={0}> Cualquiera </option>
+                                <option value={1}> Español </option>
+                                <option value={2}> Ingles </option>
+                            </select>
+
+                            <label htmlFor='configuracion-borrarcuenta'> Borra cuenta de Couple Mubis: </label>
+                            <input id='configuracion-borrarcuenta' className='configuracion-input' name='configuracion-borrarcuenta' placeholder='Escribe "BORRAR" para confirmar' value={borrarCuenta} onChange={handleBorrarCuentaChange} />
+
+                        </form>
+                    </Fragment>
+                :
+                    <Fragment>
+                        <div id='perfil-botones'>
+                            <Boton version='perfilAgregar' />
+                            <Boton version='perfilBloquear' />
+                        </div>
+                        <br />
+                        <br />
+                        <p> Bibliografia: </p>
+                        <p id='perfil-bibliografia'> {datosUsuario.bibliografia} </p>
+                        <br />
+                        <br />
+                        <br />
+                        <p> Listas Publicas: </p>
+                        <div id='perfil-conjunto-listas'>
+                            {datosUsuario.listasPublicas.map((lista, index) => {
+                                return(
+                                    <Link to={`/lista/${lista.id}`} className='no-hypervinculo' key={lista + index}>
+                                        <div className='perfil-lista'>
+                                            <img src='../src/assets/images/iconos/pelicula-roja.png' alt='Icono Pelicula' width={50} />
+                                            <p> {lista.nombre} </p>
+                                        </div>
+                                    </Link>
+                                )
+                            })}
+                        </div>
+                    </Fragment>
+                }
+
             </div>
         </main>
     </Fragment>
