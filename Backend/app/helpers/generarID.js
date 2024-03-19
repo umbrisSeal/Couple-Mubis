f0a862798516c83912d18f7733a2974e3472e973
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-03-19 15:54:50 -0600

    Added helper function to generate new hashed IDs.

---
+++ Backend/app/helpers/generarID.js
@@ -0,0 +1,14 @@
+
+const crypto = require('crypto');
+
+function generarID() {
+    const longitud = 8;
+
+    const randomBytes = crypto.randomBytes(longitud);
+    const stringBase64 = randomBytes.toString('base64');
+    const hashID = stringBase64.replace(/[+/=]/g, '');
+
+    return hashID.substring(0, longitud).toUpperCase();
+}
+
+module.exports = generarID;
