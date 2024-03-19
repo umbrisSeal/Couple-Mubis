00e0e080896c26d326087bca9d83eb625ff96281
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-03-19 16:17:55 -0600

    Added module to hash password in frontend.

---
+++ src/assets/js/hashearPassword.js
@@ -0,0 +1,36 @@
+
+/*
+    Por defecto, la salt que usaremos en el frontend sera la declarada en esta funcion.
+*/
+
+async function hashearPassword(passwordOriginal) {
+    const salt = [80,6,169,122,242,41,24,233,93,225,165,195,246,27,139,119];
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(passwordOriginal);
+
+    const concatData = new Uint8Array(data.length + salt.length);
+    concatData.set(data);
+    concatData.set(salt, data.length);
+
+    const hashBuffer = await crypto.subtle.digest('SHA-256', concatData);
+
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    const passwordHash = hashArray.map( (bite) => {
+        bite.toString(16).padStart(2, '0');
+    }).join('');
+
+
+    return passwordHash;
+}
+
+module.exports = hashearPassword;
+
+/*
+    Se debe de usar como un promise. (async-await)
+    hashPassword(stringPasswrodOriginal)
+        .then(hash => {...})
+        .catch(error => {...})
+    ;
+*/
