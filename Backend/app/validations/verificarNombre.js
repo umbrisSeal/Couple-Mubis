0393884e199f5585b91f171433bf6005f4593af0
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-03-26 15:42:22 -0600

    Added verificarNombre module for new user names.

---
+++ Backend/app/validations/verificarNombre.js
@@ -0,0 +1,14 @@
+
+/*
+    Verifica que el string proporcionado como nombre, no exceda la longitud deseada, ademas de solo incluir caracteres alfanumerios y espacios.
+*/
+
+function verificarNombre(nombre) {
+    const regexNombre = /^[a-zA-Z0-9 _-]+$/;
+
+    if(nombre.length < 3 || nombre.length > 15) return false;
+    if(!regexNombre.test(nombre)) return false;
+
+}
+
+module.exports = verificarNombre;
