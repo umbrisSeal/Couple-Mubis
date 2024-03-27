368febbdab469bc28306a65922951128a063d02e
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-03-26 21:43:40 -0600

    Edited emailExiste module to check if email already exists.

--- Backend/app/services/database/emailExiste.js
+++ Backend/app/services/database/emailExiste.js
@@ -7,10 +7,13 @@ const conexionMDB = require('./conexionMDB');
 
 async function emailExiste(email) {
     const clienteMDB = conexionMDB.conectar();
-    
+    const coleccionUsuarios = clienteMDB.db('coupleMubis').collection('usuarios');
 
+    const resultado = await coleccionUsuarios.findOne({ email: email });
 
     await conexionMDB.desconectar();
+
+    return resultado === null ? false : true;
 }
 
 
