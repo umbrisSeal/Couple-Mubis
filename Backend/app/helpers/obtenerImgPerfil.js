db50ce1fded78cd4a7398b39cfdaec50f2121d58
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-04-02 21:45:28 -0600

    Added module to get imgPerfil of any user.

---
+++ Backend/app/helpers/obtenerImgPerfil.js
@@ -0,0 +1,30 @@
+
+/*
+    Toma un numero entero indicado la imagen de perfil deseada del diccionario definido en este mismo archivo.
+*/
+
+const imagenesPerfil = {
+    1: 'anonimo.jpg',
+    2: 'mapache.jpg',
+    3: 'zorro.jpg',
+    4: 'mapache.jpg',
+    5: 'jirafa.jpg',
+    6: 'foca.jpg',
+    7: 'topo.jpg',
+    8: 'hormiga.jpg',
+    9: 'hongos.jpg',
+    10: 'pez.jpg',
+    11: 'ballena.jpg',
+    12: 'tigre.jpg',
+    13: 'hornitorrinco.jpg',
+    14: 'oso.jpg',
+    15: 'marmota.jpg',
+}
+
+function obtenerImgPerfil(imgPerfilInt) {
+    if(imgPerfilInt < 0 || imgPerfilInt > 15) return imagenesPerfil[1];
+
+    return imagenesPerfil[imgPerfilInt];
+}
+
+module.exports = obtenerImgPerfil;
--- Backend/app/models/obtenerListaModel.js
+++ Backend/app/models/obtenerListaModel.js
@@ -1,3 +1,4 @@
+const obtenerImgPerfil = require("../helpers/obtenerImgPerfil");
 const consultaUsuarios = require("../services/database/consultaUsuarios");
 const obtenerLista = require("../services/database/obtenerLista");
 const obtenerNombreUsuario = require("../services/database/obtenerNombreUsuario");
@@ -9,6 +10,8 @@ async function obtenerListaModel(listaID, userID) {
     const datosLista = await obtenerLista(listaID);
     const datosEditores = await consultaUsuarios(datosLista.editores);
     const datosLectores = await consultaUsuarios(datosLista.lectores);
+
+    // Apartir de aqui, mandar a la vista.
 
     let lista = {};
     lista.nombre = datosLista.nombre;
@@ -29,55 +32,42 @@ async function obtenerListaModel(listaID, userID) {
     lista.peliculas = datosLista.peliculas;
 
     /*
+    No se pueden usar async await o Promises, o cualquier otro tipo de funcion asyncrona dentro de ningun ciclo de iteracion que no sea For.
     lista.editores = datosLista.editores.map(async (editorID) => {
         return await obtenerNombreUsuario(editorID);
     })
     */
 
     lista.editores = [];
+    lista.lectores = [];
 
-    
-
-    for(let i = 0; i < datosLista.editores.length; i++) {
-        const objetoEditor = {nombre: await obtenerNombreUsuario(datosLista.editores[i])};
-        //console.log("Objeto Editor: ", objetoEditor);
+    for(let i = 0; i < datosEditores.length; i++) {
+        const editorId = datosEditores[i].userID;
+        const editorNombre = await obtenerNombreUsuario(editorId);
+        const editorImgPerfil = obtenerImgPerfil(datosEditores[i].imgPerfilID);  // Necesitamos obtener el listado de imagnes.
+        const objetoEditor = {
+            id: editorId,
+            nombre: editorNombre,
+            imgPerfil: editorImgPerfil,
+        };
         lista.editores.push(objetoEditor);
     }
 
-    //console.log("Datos que leimos: ", datosLista);
-    //console.log("Datos que envias: ", lista);
+    for(let i = 0; i < datosLectores.length; i++) {
+        const lectorId = datosLectores[i].userID;
+        const lectorNombre = await obtenerNombreUsuario(lectorId);
+        const lectorImgPerfil = obtenerImgPerfil(datosLectores[i].imgPerfilID);
+        const objetoLector = {
+            id: lectorId,
+            nombre: lectorNombre,
+            imgPerfil: lectorImgPerfil,
+        };
+        lista.lectores.push(objetoLector);
+    }
 
+    console.log(lista);
 
 
 }
 
 module.exports = obtenerListaModel;
-
-/*
-return {
-    nombre: 'Para maratonear!!!',
-    autoridad: 3,
-    esPublica: true,
-    editores: [
-        {id: '12RFGTH', nombre: 'Mario Bros', imgPerfil: 'anonimo.png'},
-        {id: '1GT67LO', nombre: 'Michael Jackson', imgPerfil: 'anonimo.png'},
-        {id: 'GKBIE90', nombre: 'Tea-Bean', imgPerfil: 'anonimo.png'},
-    ],
-    lectores: [
-        {id: 'EGGRGTA', nombre: 'Jose Jose', imgPerfil: 'anonimo.png'},
-        {id: 'RIGNOTA', nombre: 'Ringo Star', imgPerfil: 'anonimo.png'},
-    ],
-    peliculas: [
-        {id: 4312, vista: false, urlPoster: 'pmjiwwfT7kRJQ0ATi79upBmSOO9.jpg', titulo: 'Nosotros los Inocentes', año: 1200},
-        {id: 4321, vista: false, urlPoster: 'zaqam2RNscH5ooYFWInV6hjx6y5.jpg', titulo: 'luigi', año: 2000},
-        {id: 4321, vista: false, urlPoster: '3bhkrj58Vtu7enYsRolD1fZdja1.jpg', titulo: 'peach', año: 2012},
-        {id: 4321, vista: false, urlPoster: 'lgEXNBnFsq8oTck1C2giSvnTjzz.jpg', titulo: 'bowser', año: 2011},
-        {id: 4321, vista: true, urlPoster: 'vSzOobYVu16MogSALNg1bjTaGc.jpg', titulo: 'monkey', año: 2009},
-        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
-        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
-        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
-        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
-        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
-    ],
-}
-*/
