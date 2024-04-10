013283a502d11768d9928968d6647678a3c1d0e6
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-04-10 13:21:07 -0600

    Added comments to next endpoints.

--- Backend/app/routes/lista.js
+++ Backend/app/routes/lista.js
@@ -15,3 +15,35 @@ lista.post("/", auth, crearListaController);
 
 
 module.exports = lista;
+
+/*
+ Formato en el que viene la informacion de la lista en el body del request.
+ (Para actualizar editores manda un objeto con editores y lectores)
+ (Para actualizar peliculas manda un objeto con el arreglo peliculas y asigna los valores requeridos.).
+return {
+    nombre: 'Para maratonear!!!',
+    autoridad: 3,
+    esPublica: true,
+    editores: [
+        {id: '12RFGTH', nombre: 'Mario Bros', imgPerfil: 'anonimo.png'},
+        {id: '1GT67LO', nombre: 'Michael Jackson', imgPerfil: 'anonimo.png'},
+        {id: 'GKBIE90', nombre: 'Tea-Bean', imgPerfil: 'anonimo.png'},
+    ],
+    lectores: [
+        {id: 'EGGRGTA', nombre: 'Jose Jose', imgPerfil: 'anonimo.png'},
+        {id: 'RIGNOTA', nombre: 'Ringo Star', imgPerfil: 'anonimo.png'},
+    ],
+    peliculas: [
+        {id: 4312, vista: false, urlPoster: 'pmjiwwfT7kRJQ0ATi79upBmSOO9.jpg', titulo: 'Nosotros los Inocentes', año: 1200},
+        {id: 4321, vista: false, urlPoster: 'zaqam2RNscH5ooYFWInV6hjx6y5.jpg', titulo: 'luigi', año: 2000},
+        {id: 4321, vista: false, urlPoster: '3bhkrj58Vtu7enYsRolD1fZdja1.jpg', titulo: 'peach', año: 2012},
+        {id: 4321, vista: false, urlPoster: 'lgEXNBnFsq8oTck1C2giSvnTjzz.jpg', titulo: 'bowser', año: 2011},
+        {id: 4321, vista: true, urlPoster: 'vSzOobYVu16MogSALNg1bjTaGc.jpg', titulo: 'monkey', año: 2009},
+        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
+        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
+        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
+        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
+        {id: 4321, vista: true, urlPoster: 'bhjuTUPgY9S21yDDfRe3PeEYlYY.jpg', titulo: 'billbullet', año: 2009},
+    ],
+}
+*/
