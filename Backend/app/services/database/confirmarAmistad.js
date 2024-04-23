bb9b568ab5698bc5211238e0cd5a29e6495202e8
umbrisSeal <156217584+umbrisSeal@users.noreply.github.com>
2024-04-23 13:21:22 -0600

    Added function to confirm users friendship.

---
+++ Backend/app/services/database/confirmarAmistad.js
@@ -0,0 +1,21 @@
+
+/*
+    Funcion para simular el funcionamiento del endpoint para confirmar la amistad entre amigos. Toma userID y amigoID y verifica si son amigos mutuos.
+*/
+
+const objetoVacio = require("../../helpers/objetoVacio");
+
+async function confirmarAmistad(userID, amigoID) {
+    const datosUsuarios = await consultaUsuarios([userID, amigoID]);
+    const amigosUsuario = datosUsuarios[0].amigos;
+    const amigosAmigo = datosUsuarios[1].amigos;
+
+    if(objetoVacio(datosUsuarios[0]) || objetoVacio(datosUsuarios[1])) {
+        return false;
+    }
+
+    const sonAmigos = amigosUsuario.includes(request.amigoID) && amigosAmigo.includes(request.userID);
+    return sonAmigos;
+}
+
+module.exports = confirmarAmistad;
